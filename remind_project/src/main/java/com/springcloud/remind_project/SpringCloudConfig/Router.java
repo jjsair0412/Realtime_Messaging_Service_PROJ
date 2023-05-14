daee7cb6f03cf4cf464b432ea209b3c1d5ba3ded
jinseong <jjsair0412@naver.com>
2023-05-14 14:41:38 +0900

    modify code

--- remind_project/src/main/java/com/springcloud/remind_project/SpringCloudConfig/Router.java
+++ remind_project/src/main/java/com/springcloud/remind_project/SpringCloudConfig/Router.java
@@ -19,6 +19,8 @@ public class Router {
             .route(userStart -> userStart.path("/start")
                 .uri("http://127.0.0.1:8000")
             )
+            .route(sender -> sender.path("/sender")
+                .uri("http://127.0.0.1:8001"))
             .build();
     }
 }
--- usermanager/src/main/java/com/usermanager/usermanager/Controller/Main/MainPage.java
+++ usermanager/src/main/java/com/usermanager/usermanager/Controller/Main/MainPage.java
@@ -5,11 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.usermanager.usermanager.Domain.DTO.MainDomain;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +18,7 @@ public class MainPage {
     
     @Value("${server.port}")
     private String port;
+
 
     @GetMapping("/user")
     public String mainpageString(
